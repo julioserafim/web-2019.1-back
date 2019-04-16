cdb649b67378f615f261154e9109e2c6f909d6cf
julioserafim <serafimjuliom@gmail.com>
2019-04-16 16:35:05 -0300

    Upload Projeto Servlet

---
+++ OlaMundoServlet3/src/br/com/ufc/servlet/AdicionaPessoa.java
@@ -0,0 +1,51 @@
+package br.com.ufc.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import br.com.ufc.model.Pessoa;
+
+@WebServlet("/adicionarPessoa")
+public class AdicionaPessoa extends HttpServlet{
+	
+	@Override
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String nome = request.getParameter("nome");
+		String time = request.getParameter("time");
+		
+		Pessoa pessoa = new Pessoa();
+		pessoa.setNome(nome);
+		pessoa.setTime(time);
+		
+		PrintWriter out = response.getWriter();
+
+		out.println("<html>");
+		out.println("<head>");
+		out.println("<title> Minha servlet </title>");
+		out.println("</head>");
+		
+		out.println("<body>");
+		out.println("A pessoa " + 
+		pessoa.getNome() + "tem o time: " + 
+				pessoa.getTime());
+		
+		out.println("</body>");
+		out.println("</html>");
+		
+		//Pegar os dados do formulário
+		//Criar um objeto Pessoa
+		//Cadastrar e Mostrar na tela
+		
+		
+		
+		
+	}
+	
+	
+}
