fee3ab9daba5096cb150bd278063a67c74f4c6db
julioserafim <serafimjuliom@gmail.com>
2019-05-30 10:43:28 -0300

    CRUD COMPLETO

--- crud-springboot-2/src/main/java/com/ufc/br/controller/PessoaController.java
+++ crud-springboot-2/src/main/java/com/ufc/br/controller/PessoaController.java
@@ -1,26 +1,33 @@
 package com.ufc.br.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.ufc.br.model.Pessoa;
 import com.ufc.br.repository.PessoaRepository;
 import com.ufc.br.service.PessoaService;
 
 @Controller
+@RequestMapping("/pessoa")
 public class PessoaController {
 	
 	@Autowired
 	private PessoaService pessoaService;
 	
 	
-	@RequestMapping("/pessoa/formulario")
-	public String form() {
-		return "Formulario";
+	@RequestMapping("/formulario")
+	public ModelAndView form() {
+		ModelAndView mv = new ModelAndView("Formulario");
+		mv.addObject("pessoa", new Pessoa());
+		return mv;
 	}
 	
-	@RequestMapping("/pessoa/salvar")
+	@RequestMapping("/salvar")
 	public String salvarPessoa(Pessoa pessoa) {
 		
 			pessoaService.cadastrar(pessoa);
@@ -28,14 +35,31 @@ public class PessoaController {
 		return"OlaMundo";
 	}
 	
-	@RequestMapping("/pessoa/listar")
-	public String listarPessoas() {
+	@RequestMapping("/listar")
+	public ModelAndView listarPessoas() {
 		//trazer as pessoas do banco e mandar pra página
 		
-		return "PaginaListagem";
+		List<Pessoa> pessoas = pessoaService.listar();
+		ModelAndView mv = new ModelAndView("PaginaListagem");		
+		mv.addObject("listaDePessoas", pessoas);
+		return mv;
 	}
 	
 	
+	@RequestMapping("/excluir/{codigo}")
+	public ModelAndView excluir(@PathVariable Long codigo) {
+		pessoaService.excluir(codigo);
+		ModelAndView mv = new ModelAndView("redirect:/pessoa/listar");
+		return mv;
+	}
+	
+	@RequestMapping("/atualizar/{codigo}")
+	public ModelAndView atualizar(@PathVariable Long codigo) {
+		Pessoa pessoa = pessoaService.buscarPorId(codigo);
+		ModelAndView mv = new ModelAndView("Formulario");
+		mv.addObject("pessoa", pessoa);
+		return mv;
+	}
 	
 	
 } 
--- crud-springboot-2/src/main/java/com/ufc/br/service/PessoaService.java
+++ crud-springboot-2/src/main/java/com/ufc/br/service/PessoaService.java
@@ -1,5 +1,7 @@
 package com.ufc.br.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -16,6 +18,25 @@ public class PessoaService {
 	public void cadastrar(Pessoa pessoa) {
 		pessoaRepository.save(pessoa);
 	}
+
+
+	public List<Pessoa> listar() {
+		return pessoaRepository.findAll();
+	}
+
+
+	public void excluir(Long codigo) {
+		pessoaRepository.deleteById(codigo);
+		
+	}
+
+
+	public Pessoa buscarPorId(Long codigo) {
+		return pessoaRepository.getOne(codigo);
+	}
+
+
+	
 	
 	
 
